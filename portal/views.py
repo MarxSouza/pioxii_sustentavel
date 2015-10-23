b80890eb3e60a1c076557799c6764d1cdd408557
MarxSouza <karlmarx12crvg@gmail.com>
2015-10-23 00:37:19 -0300

    final commit

--- portal/views.py
+++ portal/views.py
@@ -11,7 +11,7 @@ def formulario(request):
         if form.is_valid():
             pes = form.save()
             pes.save()
-            return HttpResponseRedirect("/pesquisa/grafico/?form=save")
+            return HttpResponseRedirect("/grafico.py")
     else: 
         form = forms.PesquisaForm()
     return render(request, 'portal/formulario.html', {
